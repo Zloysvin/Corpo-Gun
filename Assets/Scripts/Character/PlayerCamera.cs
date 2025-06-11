33a1117a9fe806e4c205aefef1886dac6fdca148
Resben <bendavidhawkins@gmail.com>
2025-06-11 21:59:29 +0800

    [Fix] Improved camera

--- Assets/Scripts/Character/PlayerCamera.cs
+++ Assets/Scripts/Character/PlayerCamera.cs
@@ -9,18 +9,27 @@ public class PlayerCamera : MonoBehaviour
 {
     [SerializeField] private float sensitivity = 0.1f;
 
-    private Vector3 _eularAngles;
+    private float _yaw;
+    private float _pitch;
 
     public void Initialize(Transform target)
     {
         transform.position = target.position;
-        transform.eulerAngles = _eularAngles = target.eulerAngles;
+
+        Vector3 angles = transform.eulerAngles;
+        _yaw = angles.y;
+        _pitch = angles.x;
+        
+        transform.rotation = Quaternion.Euler(_pitch, _yaw, 0f);
     }
 
     public void UpdateRotation(CameraInput input)
     {
-        _eularAngles += new Vector3(-input.Look.y, input.Look.x) * sensitivity;
-        transform.eulerAngles = _eularAngles;
+        _yaw += input.Look.x * sensitivity;
+        _pitch -= input.Look.y * sensitivity;
+        _pitch = Mathf.Clamp(_pitch, -89f, 89f);
+
+        transform.rotation = Quaternion.Euler(_pitch, _yaw, 0f);
     }
 
     public void UpdatePosition(Transform target)
